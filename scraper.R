21fda1af246e4cbd8369deb6303f3996f5c95173
jdm79 <james.malvern@gmail.com>
2020-03-28 21:18:50 +0000

    adds black background, random-light text

--- scraper.R
+++ scraper.R
@@ -11,42 +11,49 @@ library(stringr)
 library(rtweet)
 library(wordcloud2)
 
+#Create twitter token
+#replace with your values
 create_token(
   app = "PLACE YOUR APP NAME HERE",
   consumer_key = "PLACE YOUR CONSUMER KEY HERE",
   consumer_secret = "PLACE YOUR CONSUMER SECRET HERE")
 
+############# Handmaiden's Tale Tweets #############
+
 #Grab tweets - note: reduce to 1000 if it's slow
+?search_tweets
 hmt <- search_tweets(
-  "#HandmaidsTale", n = 2000, include_rts = FALSE
+  "#5G", n = 2000, include_rts = FALSE
 )
 
-# have a look at the data
+#Look at tweets
 head(hmt)
 dim(hmt)
 hmt$text
 
-# Unnest the words - code via Tidy Text
+#Unnest the words - code via Tidy Text
+
 hmtTable <- hmt %>% 
   unnest_tokens(word, text)
 
-# remove stop words - aka typically very common words such as "the", "of" etc
+#remove stop words - aka typically very common words such as "the", "of" etc
+
 data(stop_words)
+
 hmtTable <- hmtTable %>%
   anti_join(stop_words)
 
 #do a word count
+
 hmtTable <- hmtTable %>%
   count(word, sort = TRUE) 
 hmtTable 
 
 #Remove other nonsense words
 hmtTable <-hmtTable %>%
-  filter(!word %in% c('t.co', 'https', 'handmaidstale', "handmaid's", 'season', 'episode', 'de', 'handmaidsonhulu',  'tvtime', 
-                      'watched', 'watching', 'watch', 'la', "it's", 'el', 'en', 'tv',
+  filter(!word %in% c('t.co', 'https', 'de', 'la', "it's", 'el', 'en',
                       'je', 'ep', 'week', 'amp'))
 
-wordcloud2(hmtTable, size=0.7)
 
-
-
+#wordcloud2
+wordcloud2(hmtTable, backgroundColor = "black", color = "random-light")
